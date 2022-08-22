cdf4104011604993fa0e913f085e97bc8e88890f
Paulo Milhorato <Paulo@MacBookAir.local>
2022-08-22 01:11:47 -0300

    update projeto

--- src/main/java/br/com/parking/controllers/ParkingSpotController.java
+++ src/main/java/br/com/parking/controllers/ParkingSpotController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,4 +88,24 @@ public class ParkingSpotController {
 		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot Deleted Successfully");
 		
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id,
+													@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+		
+		Optional<ParkingSpot> parkingSpotOptional = parkingSpotServices.findById(id);
+		if (!parkingSpotOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found!");
+		}
+		
+		ParkingSpot parkingSpot = parkingSpotOptional.get();
+		BeanUtils.copyProperties(parkingSpotDto, parkingSpot);
+		parkingSpot.setId(parkingSpotOptional.get().getId());
+		parkingSpot.setRegistrationDate(parkingSpotOptional.get().getRegistrationDate());
+		
+		
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotOptional.get());
+		
+	}
+	
 }
