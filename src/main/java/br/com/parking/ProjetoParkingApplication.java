244e9005edae808004ef559389171b91eba755d0
Paulo Milhorato <Paulo@MacBookAir.local>
2022-08-21 23:09:55 -0300

    Atualizacao

---
+++ src/main/java/br/com/parking/ProjetoParkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoParkingApplication.class, args);
+	}
+
+}
